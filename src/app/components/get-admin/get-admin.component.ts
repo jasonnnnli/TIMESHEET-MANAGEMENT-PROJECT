7e72bf6bbfb106e11447b72043dda479dcac0b19
jinlongli <38239621+jasonnnnli@users.noreply.github.com>
2021-04-07 00:10:19 -0400

    Update get-admin.component.ts

--- src/app/components/get-admin/get-admin.component.ts
+++ src/app/components/get-admin/get-admin.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { MockDataService } from 'src/app/services/mock-data.service';
 @Component({
   selector: 'app-get-admin',
   templateUrl: './get-admin.component.html',
   styleUrls: ['./get-admin.component.css']
 })
 export class GetAdminComponent implements OnInit {
+  users: any;
+  errorMsg: any;
 
-  constructor() { }
+  constructor(private mockData: MockDataService, private route: ActivatedRoute, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+ 
+    this.mockData.getUsers().subscribe(
+      (data) => {
+        this.users = data; 
+        console.log(data); 
+      },
+      (error) => {this.errorMsg = error; console.log(error); }
+    );
   }
 
+  goBack(){
+      this.router.navigate(['/home']);
+  }
 }
